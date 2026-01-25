effff62cb195de4aae025f57202e9f906a86b38e
ZhaoYukuan <zhaoyukuan95@gmail.com>
2026-01-25 23:01:03 +0800

    .

--- react-test/src/App.tsx
+++ react-test/src/App.tsx
@@ -64,8 +64,11 @@ const Candle = memo(() => {
   const snap = useSnapshot(state)
   const chart: { current: any } = useRef(null)
   const data: { current: any } = useRef(null)
+  const [maPrice, setMaPrice] = useState({ ma8Price: 0, ma288Price: 0, })
   const [isShowLoading, setIsShowLoading] = useState(true)
   const flexStyle = FlexStyle()
+  const ma8Color = '#4FC1FF'
+  const ma288Color = '#e82ac2'
 
   function mapCandle(data: Array<any>): Array<any> {
     return data.map((item: any) => {
@@ -95,15 +98,19 @@ const Candle = memo(() => {
   function setData(data: Array<any>) {
     const ma8Data = calculateMA(data, 8)
     const ma288Data = calculateMA(data, 288)
+    setMaPrice({
+      ma8Price: ma8Data.at(-1)?.value ?? 0,
+      ma288Price: ma288Data.at(-1)?.value ?? 0
+    })
     const ma8Series = chart.current.addSeries(LineSeries, {
-      color: '#4FC1FF', lineWidth: 1, lastValueVisible: false,
+      color: ma8Color, lineWidth: 1, lastValueVisible: false,
       priceLineVisible: false, crosshairMarkerVisible: false,
       priceFormat: {
         type: 'custom', minMove: 0.01, formatter: (price: any) => Number.parseInt(price),
       },
     })
     const ma288Series = chart.current.addSeries(LineSeries, {
-      color: '#e82ac2', lineWidth: 1, lastValueVisible: false,
+      color: ma288Color, lineWidth: 1, lastValueVisible: false,
       priceLineVisible: false, crosshairMarkerVisible: false,
       priceFormat: {
         type: 'custom', minMove: 0.01, formatter: (price: any) => Number.parseInt(price),
@@ -143,8 +150,8 @@ const Candle = memo(() => {
   }
 
   function getBackgroundColor(): string {
-    const n = snap.isLight ? 255 : 1
-    return `rgba(${n}, ${n}, ${n}, 0.2)`
+    const color = snap.isLight ? `255,255,255` : `31,38,47`
+    return `rgba(${color}}, 0.2)`
   }
 
   const style = {
@@ -165,7 +172,17 @@ const Candle = memo(() => {
       position: absolute;
       top: 0;
       right: 70px;
-    `
+    `,
+    legend: css`
+      z-index: 2;
+      position: absolute;
+      top: 0;
+      left: 10px;
+      font-size: 12px;
+      display: flex;
+      gap: 10px;
+      user-select: none;
+    `,
   }
 
   useEffect(() => {
@@ -189,8 +206,14 @@ const Candle = memo(() => {
           } else {
             data.current[data.current.length - 1] = candle
           }
-          ma8Series.update(calculateMA(data.current, 8).at(-1))
-          ma288Series.update(calculateMA(data.current, 288).at(-1))
+          const ma8Data = calculateMA(data.current, 8).at(-1)
+          const ma288Data = calculateMA(data.current, 288).at(-1)
+          ma8Series.update(ma8Data)
+          ma288Series.update(ma288Data)
+          setMaPrice({
+            ma8Price: ma8Data?.value ?? 0,
+            ma288Price: ma288Data?.value ?? 0,
+          })
           candlestickSeries.update(data.current.at(-1))
         })
       } catch {
@@ -200,7 +223,7 @@ const Candle = memo(() => {
     init()
     const darkTheme = {
       layout: {
-        background: { color: '#141414' },
+        background: { color: '#1F262F' },
         textColor: '#d1d4dc',
       },
       grid: {
@@ -243,21 +266,27 @@ const Candle = memo(() => {
       {(isShowLoading) ?
         <div className={cx(style.loadingC, flexStyle.fcc)}>
           <Loading width={30} border={3}></Loading>
-        </div> :
-        <div className={cx(style.button)}>
-          {snap.isShowCandle ?
-            <Button onClick={() => state.isShowCandle = !snap.isShowCandle} style={{ width: '30px', height: '30px', padding: '0' }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8m7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0m-.5 11.707-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0z" />
-            </svg></Button> :
-            <Button onClick={() => state.isShowCandle = !snap.isShowCandle} style={{ width: '30px', height: '30px', padding: '0' }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10" />
-            </svg></Button>
-          }
-        </div>}
+        </div> : (<>
+          <div className={cx(style.button)}>
+            {snap.isShowCandle ?
+              <Button onClick={() => state.isShowCandle = !snap.isShowCandle} style={{ width: '30px', height: '30px', padding: '0' }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8m7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0m-.5 11.707-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0z" />
+              </svg></Button> :
+              <Button onClick={() => state.isShowCandle = !snap.isShowCandle} style={{ width: '30px', height: '30px', padding: '0' }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10" />
+              </svg></Button>
+            }
+          </div>
+          <div className={cx(style.legend)}>
+            <span style={{ color: snap.isLight ? '#000000' : '#FFFFFF' }}>15m</span>
+            <span style={{ color: ma8Color }}>{'MA(8): ' + Number(maPrice.ma8Price).toLocaleString('en-US')}</span>
+            <span style={{ color: ma288Color }}>{'MA(288): ' + Number(maPrice.ma288Price).toLocaleString('en-US')}</span>
+          </div>
+        </>)
+      }
     </div>
   </>)
 })
-
 
 function initWebsocket() {
   const socket = new WebSocket('wss://api.hyperliquid.xyz/ws')
